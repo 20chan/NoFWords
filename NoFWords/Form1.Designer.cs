3772da25a9cb2ac7f01dd1ce383e4e2db714f8a5
phillyai <sonagi.philly@gmail.com>
2018-05-25 16:12:55 +0900

    초기 파일 추가

--- NoFWords/Form1.Designer.cs
+++ NoFWords/Form1.Designer.cs
@@ -29,12 +29,30 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "욕하지망";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
     }
 }
 
--- NoFWords/Form1.cs
+++ NoFWords/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
+
+using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
 
 namespace NoFWords
 {
@@ -15,6 +19,33 @@ namespace NoFWords
         public Form1()
         {
             InitializeComponent();
+
+            try
+            {
+                var sre = new SpeechRecognitionEngine(new CultureInfo("ko-KR"));
+                var cmds = new Choices();
+                cmds.Add("씨발", "개새끼", "미친", "나쁜놈", "노답", "쓰레기");
+                var builder = new GrammarBuilder();
+                builder.Append(cmds);
+                //builder.AppendWildcard();
+                var g = new Grammar(builder);
+                
+                sre.LoadGrammar(g);
+                sre.SpeechRecognized += Sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "에러");
+                Application.Exit();
+            }
+        }
+
+        private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(0, "욕하지망", $"{e.Result.Text} 이라 욕하면 안됭!!", ToolTipIcon.Warning);
         }
     }
 }
